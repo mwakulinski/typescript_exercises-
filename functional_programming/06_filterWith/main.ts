1afd378967b7a2e5306dc22550ff515888dac4ca
Michal Wakulinski <michal.wakulinski1@gmail.com>
2022-03-08 06:49:49 +0100

    add filterwith

--- functional_programming/06_filterWith/main.ts
+++ functional_programming/06_filterWith/main.ts
@@ -1,3 +1,12 @@
 // tak aby:
 // - od 0 do 2 znaków w phrase zwracało pusty array,
 // - a powyżej 2 ma filtrować po każdej wartości typu string lub number w obiekcie
+
+import { data } from "./data";
+type Data = typeof data;
+
+const filterWith = (array: Data[], phraseToFind: string) => {
+  return array.filter((seekData) => {
+    return chckIfHavePhrase(seekData, phraseToFind);
+  });
+};
