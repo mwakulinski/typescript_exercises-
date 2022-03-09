896eeaa54deba19bdaec95a4ce90ac4b1eee19e3
Michal Wakulinski <michal.wakulinski1@gmail.com>
2022-03-09 12:13:15 +0100

    add Group

--- OOP/01_AddressBook/group.ts
+++ OOP/01_AddressBook/group.ts
@@ -3,13 +3,44 @@
 
 import { Contact } from "./contact";
 import { v4 as uuidv4 } from "uuid";
+import { Validator } from "./validator";
 
 interface IGroup {
   name: string;
   contactsList: Contact[];
   id: string;
-  changeName: (newName: string) => string;
+  changeName: (newName: string) => void;
   addContact: (name: string, surname: string, email: string) => void;
   deleteContact: (contactId: string) => void;
   checkIfCintactExists: (phrase: string) => boolean;
+}
+
+export class Group implements IGroup {
+  public contactsList: Contact[] = [];
+  readonly id: string = uuidv4();
+  constructor(public name: string) {
+    Validator.throwIfEmptyString(name);
+  }
+
+  changeName(newName: string) {
+    Validator.throwIfEmptyString(newName);
+    this.name = newName;
+  }
+
+  addContact(name: string, surname: string, email: string) {
+    this.contactsList.push(new Contact(name, surname, email));
+  }
+
+  deleteContact(contactId: string) {
+    if (!this.checkIfCintactExists(contactId)) {
+      throw new Error("Such a contact does not exist in this group");
+    }
+    this.contactsList = this.contactsList.filter(({ id }) => id !== contactId);
+  }
+
+  checkIfCintactExists(phrase: string) {
+    return this.contactsList.some((contact) =>
+      contact.checkIfHavaPhrase(phrase)
+    );
+  }
 }
