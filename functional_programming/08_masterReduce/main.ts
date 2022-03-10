3e57cf4315420cfefea44c7784a8c8302c36ff52
Michal Wakulinski <michal.wakulinski1@gmail.com>
2022-03-10 13:35:40 +0100

    updapte everyFn

--- functional_programming/08_masterReduce/main.ts
+++ functional_programming/08_masterReduce/main.ts
@@ -1,24 +1,22 @@
 const mapFn = <T, K>(array: T[], callback: (data: T) => K) => {
-  const newArr: T[] = [];
   return array.reduce((accumulator: (T | K)[], current: T) => {
     accumulator.push(callback(current));
     return accumulator;
-  }, newArr);
+  }, [] as T[]);
 };
 
-const filterFn = <T>(array: T[], callback: (data: T) => T | undefined) => {
-  const newArr: T[] = [];
+const filterFn = <T>(array: T[], callback: (data: T) => boolean) => {
   return array.reduce((accumulator: T[], current: T) => {
     if (callback(current)) {
       accumulator.push(current);
     }
     return accumulator;
-  }, newArr);
+  }, [] as T[]);
 };
 
 const everyFn = <T>(array: T[], callback: (data: T) => boolean) => {
   return array.reduce((accumulator: boolean, current: T) => {
-    if (callback(current)) {
+    if (!callback(current)) {
       return false;
     }
     return accumulator;
@@ -38,10 +36,6 @@ const multiplyByTwo = (numb: number) => {
   return numb * 2;
 };
 
-const filterEven = (numb: number) => {
-  if (numb % 2 === 0) return numb;
-};
-
 const isEven = (numb: number) => {
   return numb % 2 === 0;
 };
@@ -51,7 +45,7 @@ const numbArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const mapFunc = mapFn(numbArr, multiplyByTwo);
 console.log(mapFunc);
 
-const filterArr = filterFn(numbArr, filterEven);
+const filterArr = filterFn(numbArr, isEven);
 console.log(filterArr);
 
 const areAllEven = everyFn(numbArr, isEven);
